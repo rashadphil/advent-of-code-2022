f9aaac5bcd5268a00609512569ebb6bb09aca579
Rashad Philizaire <rashadphilizaire@gmail.com>
2022-12-17 12:28:32 -0500

    day6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,12 +1,44 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::hash::Hash;
+use std::{collections::HashSet, fs::read_to_string};
 
 ///////////////////////////////////////////////////////////////////////////////
+///
+
+fn has_unique_elems<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Hash + Eq,
+{
+    let mut hashset = HashSet::new();
+    iter.into_iter().all(|x| hashset.insert(x))
+}
+
+pub fn part1(chars: &[char]) -> u32 {
+    let iter = chars.windows(4);
+    for (idx, window) in iter.enumerate() {
+        if has_unique_elems(window) {
+            return (idx + 4) as u32;
+        }
+    }
+    0
+}
+pub fn part2(chars: &[char]) -> u32 {
+    let iter = chars.windows(14);
+    for (idx, window) in iter.enumerate() {
+        if has_unique_elems(window) {
+            return (idx + 14) as u32;
+        }
+    }
+    0
+}
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("inputs/day06.txt").unwrap();
+    let chars: Vec<char> = input.chars().collect();
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let sol1 = part1(&chars);
+    let sol2 = part2(&chars);
+
+    (Solution::U32(sol1), Solution::U32(sol2))
 }
