1c64147f4d8ae86c31c550b5d4a07b4e5998b85c
Rashad Philizaire <rashadphilizaire@gmail.com>
2022-12-18 13:50:31 -0500

    day7 and day8

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,12 +1,125 @@
 use crate::{Solution, SolutionPair};
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string, iter::Peekable, path::PathBuf, str::Lines};
+
+use itertools::Itertools;
 
 ///////////////////////////////////////////////////////////////////////////////
+///
+
+impl Command {
+    fn from_line(line: &str) -> Self {
+        let split: Vec<&str> = line.split(' ').collect();
+        match split[1] {
+            "cd" => Command::Cd(split[2].into()),
+            "ls" => Command::Ls,
+            _ => panic!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Command {
+    Cd(String),
+    Ls,
+}
+
+#[derive(Debug)]
+enum Entry {
+    Dir(String),
+    File(u64, String),
+}
+
+#[derive(Debug)]
+enum Line {
+    Command(Command),
+    Entry(Entry),
+}
+
+fn parse_entry(line: &str) -> Entry {
+    let mut split = line.split(' ');
+    let (first, sec) = (split.next().unwrap(), split.next().unwrap());
+
+    match (first, sec) {
+        ("dir", name) => Entry::Dir(name.into()),
+        (size_str, name) => Entry::File(size_str.parse().unwrap(), name.into()),
+    }
+}
+
+fn parse_command(line: &str) -> Command {
+    let split: Vec<_> = line.split(' ').collect();
+    match split[1] {
+        "cd" => Command::Cd(split[2].into()),
+        "ls" => Command::Ls,
+        _ => unreachable!(),
+    }
+}
+
+fn parse_line(line: &str) -> Line {
+    match line.chars().next().unwrap() {
+        '$' => Line::Command(parse_command(line)),
+        _ => Line::Entry(parse_entry(line)),
+    }
+}
+
+fn part1(sizes: &[u64]) -> u64 {
+    sizes.iter().filter(|s| **s <= 100000).sum()
+}
+
+fn part2(sizes: &[u64]) -> u64 {
+    let total_space = 70_000_000;
+    let min_free_space = 30_000_000;
+    let free_space = total_space - sizes[0];
+
+    let needed_space = min_free_space - free_space;
+
+    *sizes
+        .iter()
+        .sorted()
+        .find(|sz| **sz >= needed_space)
+        .unwrap()
+}
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("inputs/day07.txt").unwrap();
+    let lines: Vec<Line> = input.lines().map(parse_line).collect();
+    let mut sizes = [0; 200];
+
+    // Each index is a level
+    // each index in a level is a different directory
+    let mut sizes = [0; 200];
+    let mut path_folders = [0; 200];
+    let mut level = 0;
+    let mut folder_ct = 0;
+
+    let mut line_iter = lines.into_iter();
+
+    for line in line_iter {
+        match line {
+            Line::Command(c) => match c {
+                Command::Cd(path) => match path.as_str() {
+                    ".." => level -= 1,
+                    _ => {
+                        path_folders[level] = folder_ct;
+                        level += 1;
+                        folder_ct += 1;
+                    }
+                },
+                Command::Ls => continue,
+            },
+            Line::Entry(e) => match e {
+                Entry::Dir(_) => continue,
+                Entry::File(size, _name) => {
+                    for i in 0..level {
+                        let folder = path_folders[i];
+                        sizes[folder] += size
+                    }
+                }
+            },
+        }
+    }
+
+    let sol1: u64 = part1(&sizes);
+    let sol2: u64 = part2(&sizes);
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,12 +1,127 @@
+use itertools::{enumerate, Itertools};
+use take_until::TakeUntilExt;
+
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
+///
+///
+
+pub fn fill_visible(tree_matrix: &Vec<Vec<u32>>, visible_matrix: &mut Vec<Vec<bool>>) {
+    let num_rows = tree_matrix.len();
+    let num_cols = tree_matrix[0].len();
+    for c in 0..num_cols {
+        let mut inner_closure = |r: usize, height: u32, max_h: &mut Option<u32>| {
+            if let Some(h) = max_h {
+                if height > *h {
+                    *max_h = Some(height);
+                    visible_matrix[r][c] = true;
+                }
+            } else {
+                *max_h = Some(height);
+                visible_matrix[r][c] = true;
+            }
+        };
+
+        let forward = 0..num_rows;
+        let backward = (0..num_rows).rev();
+
+        let mut max_h = None;
+        for r in forward {
+            let height = tree_matrix[r][c];
+            inner_closure(r, height, &mut max_h)
+        }
+
+        let mut max_h = None;
+        for r in backward {
+            let height = tree_matrix[r][c];
+            inner_closure(r, height, &mut max_h)
+        }
+    }
+
+    // row by row, forward and backwards
+    for (r, row) in enumerate(tree_matrix) {
+        let mut inner_closure = |c: usize, height: u32, max_h: &mut Option<u32>| {
+            if let Some(h) = max_h {
+                if height > *h {
+                    *max_h = Some(height);
+                    visible_matrix[r][c] = true;
+                }
+            } else {
+                *max_h = Some(height);
+                visible_matrix[r][c] = true;
+            }
+        };
+
+        let forward = enumerate(row);
+        let backward = enumerate(row).rev();
+
+        let mut max_h = None;
+        for (c, height) in forward {
+            inner_closure(c, *height, &mut max_h)
+        }
+
+        let mut max_h = None;
+        for (c, height) in backward {
+            inner_closure(c, *height, &mut max_h)
+        }
+    }
+}
+
+pub fn fill_scenic(tree_matrix: &Vec<Vec<u32>>, scenic_scores: &mut Vec<Vec<u32>>) {
+    let num_rows = tree_matrix.len();
+    let num_cols = tree_matrix[0].len();
+    for r in 0..num_rows {
+        for c in 0..num_cols {
+            let height = tree_matrix[r][c];
+
+            let up_score = (0..r)
+                .rev()
+                .take_until(|up_r| tree_matrix[*up_r][c] >= height)
+                .count();
+
+            let down_score = (r + 1..num_rows)
+                .take_until(|down_r| tree_matrix[*down_r][c] >= height)
+                .count();
+
+            let left_score = (0..c)
+                .rev()
+                .take_until(|left_c| tree_matrix[r][*left_c] >= height)
+                .count();
+
+            let right_score = (c + 1..num_cols)
+                .take_until(|right_c| tree_matrix[r][*right_c] >= height)
+                .count();
+
+            scenic_scores[r][c] = (up_score * down_score * left_score * right_score) as u32;
+        }
+    }
+}
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = read_to_string("inputs/day08.txt").unwrap();
+    let tree_matrix = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect::<Vec<Vec<u32>>>();
+
+    let rows = tree_matrix.len();
+    let cols = tree_matrix[0].len();
+
+    let mut visible_matrix = vec![vec![false; cols]; rows];
+    fill_visible(&tree_matrix, &mut visible_matrix);
+
+    let mut scenic_matrix = vec![vec![0; cols]; rows];
+    fill_scenic(&tree_matrix, &mut scenic_matrix);
+
+    let sol1: u64 = visible_matrix.iter().flatten().filter(|b| **b).count() as u64;
+
+    let sol2: u64 = *scenic_matrix.iter().flatten().max().unwrap() as u64;
 
     (Solution::U64(sol1), Solution::U64(sol2))
 }
